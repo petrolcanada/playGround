fbc27cee7b9d40694988de6b049a1397deec8efd
petrolcanada <lzhyxyz@gmail.com>
2018-09-16 12:28:07 -0400

    a test js file

--- test.js
+++ test.js
@@ -1,2 +1,9 @@
-const js = request('js')
-this is a second line
+// const js = require('js')
+
+// console.log("hello world")
+
+setTimeout(function(){
+  console.log("1 second has passed")
+}, 1000);
+
+console.log(__dirname);
